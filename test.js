89f3266a168ff276293887bc1ea222832b346382
Amare-Anagaw <scholarshiponly4ever@gmail.com>
2020-07-02 22:45:37 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+describe("All Test", function() {
+
+  
+	it("the string after removed the bann word:", function() {
+    assert.equal("I am not smart".filter("not"), "I am smart");
+   
+  });
+	it("Array after sorted ::", function() {
+    assert.equal("I am not smart".filter("not"), "I am smart");
+   
+  });
+	it("After an object called Teacher derived from the Person class ::", function() {
+    assert.equal(teacher.teach(" WAP"), "Amare Anagaw is now teaching WAP");
+   
+  });
+	
+	
+/*it("Array after sorted :", function() {
+    assert.equal([6, 4, 0, 3, -2, 1].bubbleSort(), [ -2, 0, 1, 3, 4, 6 ]);
+   
+  });	*/
+});
